38cc7f1807cc764c67d7e2e9338e62b33c01df5e
julia-ing <ywchoi0625@gmail.com>
2021-03-23 21:23:28 +0900

    Modify profile

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nickname = models.CharField(max_length=50, unique=True)
-    avatar = models.ImageField(upload_to="image", default='default_img/default_img.jpg')
+    avatar = models.ImageField(upload_to="image", default='image/default_img.jpg')
     website = models.URLField(null=True, blank=True)
     bio = models.TextField(blank=True)
     phone = models.CharField(max_length=50, null=True, blank=True)
