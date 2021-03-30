a9efc57d67bd01d82c0a93f83727335f5caa55c9
julia-ing <ywchoi0625@gmail.com>
2021-03-30 22:49:38 +0900

    모델 수정, 3주차 기본 세팅

--- api/models.py
+++ api/models.py
@@ -3,7 +3,15 @@ from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
 
 
-class Profile(models.Model):
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Profile(Base):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nickname = models.CharField(max_length=50, unique=True)
     avatar = models.ImageField(upload_to="image", default='image/default_img.jpg')
@@ -15,46 +23,36 @@ class Profile(models.Model):
         return self.nickname
 
 
-class Post(models.Model):
+class Post(Base):
     writer = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='writer')
     tags = TaggableManager(blank=True)
     location = models.CharField(max_length=100, null=True, blank=True)
     can_comment = models.BooleanField(default=True)
     text = models.TextField(null=True)
-    like_users = models.ManyToManyField(
-        'Profile',
-        through='Like',
-        related_name='like_posts'
-    )
-    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return '{} : {}'.format(self.writer, self.text)
-
-    def like_count(self):
-        return self.like_users.all().count()
 
     class Meta:
         ordering = ['-created_at']
 
 
-class File(models.Model):
+class File(Base):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     file = models.FileField(upload_to="image")
 
 
-class Comment(models.Model):
+class Comment(Base):
     commenter = models.ForeignKey(Profile, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    root = models.ForeignKey('self', null=True, related_name='rootcomment', on_delete=models.CASCADE)
+    root = models.ForeignKey('self', null=True, related_name='rootcomment', on_delete=models.CASCADE, blank=True)
     text = models.TextField(max_length=500)
-    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return '{} : {}'.format(self.commenter, self.text)
 
 
-class Follow(models.Model):
+class Follow(Base):
     follower = models.ForeignKey(Profile, related_name='follower', on_delete=models.CASCADE)
     following = models.ForeignKey(Profile, related_name='following', on_delete=models.CASCADE)
 
@@ -62,10 +60,9 @@ class Follow(models.Model):
         return '{} -> {}'.format(self.follower, self.following)
 
 
-class Like(models.Model):
+class Like(Base):
     liker = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, related_name='like_posts', on_delete=models.CASCADE)
-    liked_at = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
 
     def __str__(self):
         return '{} -> {}'.format(self.liker, self.post)
--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'taggit',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
