2673a7e38d2c43239b9e495997cd8a52ecf29448
julia-ing <ywchoi0625@gmail.com>
2021-04-08 15:08:39 +0900

    Add profile APIView

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from api import views
 urlpatterns = [
     path('posts/', views.PostList.as_view(), name='posts'),
     path('posts/<int:pk>', views.PostDetail.as_view(), name='post_detail'),
+    path('users/', views.ProfileList.as_view(), name='users'),
+    path('users/<int:pk>', views.ProfileDetail.as_view(), name='user_detail')
 ]
--- api/views.py
+++ api/views.py
@@ -32,6 +32,40 @@ from rest_framework import status
 #         return JsonResponse(serializer.data, safe=False)
 
 
+class ProfileList(APIView):
+    def get(self, request, format=None):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProfileDetail(APIView):
+    def get(self, request, pk, format=None):
+        profile = Profile.objects.get(pk=pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        profile = Profile.objects.get(pk=pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        profile = Profile.objects.get(pk=pk)
+        profile.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 class PostList(APIView):
     def get(self, request, format=None):
         posts = Post.objects.all()
