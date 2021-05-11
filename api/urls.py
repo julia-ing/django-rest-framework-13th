7b0a6c21951047a40e00b0b2401a642034cb648c
julia-ing <ywchoi0625@gmail.com>
2021-05-11 23:29:27 +0900

    basic viewset

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,22 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
+from rest_framework import routers
+from .views import PostViewSet, ProfileViewSet
 
-urlpatterns = [
-    path('posts/', views.PostList.as_view(), name='posts'),
-    path('posts/<int:pk>', views.PostDetail.as_view(), name='post_detail'),
-    path('users/', views.ProfileList.as_view(), name='users'),
-    path('users/<int:pk>', views.ProfileDetail.as_view(), name='user_detail')
-]
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet)   # register()함으로써 두 개의 url 생성
+router.register(r'users', ProfileViewSet)
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = router.urls
+
+#  CBV
+# urlpatterns = [
+#     path('posts/', views.PostList.as_view(), name='posts'),
+#     path('posts/<int:pk>', views.PostDetail.as_view(), name='post_detail'),
+#     path('users/', views.ProfileList.as_view(), name='users'),
+#     path('users/<int:pk>', views.ProfileDetail.as_view(), name='user_detail')
+# ]
+# urlpatterns = format_suffix_patterns(urlpatterns)
+
 
--- api/views.py
+++ api/views.py
@@ -4,8 +4,20 @@ from api.serializers import ProfileSerializer, PostSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+
+
+# FBV
 # @csrf_exempt
 # def post_list(request):
 #     """
@@ -33,82 +45,43 @@ from rest_framework import status
 #         return JsonResponse(serializer.data, safe=False)
 
 
-class ProfileList(APIView):
-    def get(self, request, format=None):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = ProfileSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ProfileDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Post.objects.get(pk=pk)
-        except Profile.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        profile = self.get_object(pk)
-        profile.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class PostList(APIView):
-    def get(self, request, format=None):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class PostDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
+# CBV
+# class ProfileList(APIView):
+#     def get(self, request, format=None):
+#         profiles = Profile.objects.all()
+#         serializer = ProfileSerializer(profiles, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, format=None):
+#         serializer = ProfileSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class ProfileDetail(APIView):
+#     def get_object(self, pk):
+#         try:
+#             return Post.objects.get(pk=pk)
+#         except Profile.DoesNotExist:
+#             raise Http404
+#
+#     def get(self, request, pk, format=None):
+#         profile = self.get_object(pk)
+#         serializer = ProfileSerializer(profile)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk, format=None):
+#         profile = self.get_object(pk)
+#         serializer = ProfileSerializer(profile, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk, format=None):
+#         profile = self.get_object(pk)
+#         profile.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+#
